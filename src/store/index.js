dc9b18c6a475180ec6b2ea357f545928b981f9ea
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2024-09-01 20:22:04 +0300

    Работа с библиотекой redux-persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,20 @@
-const { createStore } = require("redux");
-const { rootReducer } = require("./root-reducer");
+import { createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' 
+import { rootReducer } from './root-reducer';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['filters']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const store = createStore(
-    rootReducer,
+    persistedReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export {store};
+export const persistor = persistStore(store);
